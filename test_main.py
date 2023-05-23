27978e01ac77820d8d3e9f71533b40f2897d6918
brianmchugh <brianmchugh02@gmail.com>
2023-05-22 21:32:08 -0400

    Test cases for subtract included

--- test_main.py
+++ test_main.py
@@ -33,5 +33,34 @@ class TestCalculator(unittest.TestCase):
             data = [None, 1]
             self.calc.add(*data)
 
+    def test_subtract(self):
+        data = [5, 3]
+        # The * operator is used to unpack the contents of data as arguments to the add method.
+        result = self.calc.subtract(*data)
+        self.assertEqual(result, 2, "Failed on subtracting two positive numbers.")
+
+        data = [-1, 1]
+        result = self.calc.subtract(*data)
+        self.assertEqual(result, -2, "Failed on subtracting a negative and a positive number.")
+
+        data = [0, 0]
+        result = self.calc.subtract(*data)
+        self.assertEqual(result, 0, "Failed on subtracting two zeros.")
+
+        data = [-1, -1]
+        result = self.calc.subtract(*data)
+        self.assertEqual(result, 0, "Failed on subtracting two negative numbers.")
+
+        # Testing with a string input
+        with self.assertRaises(ValueError):
+            data = ['a', 1]
+            self.calc.subtract(*data)
+
+        # Testing with None input
+        with self.assertRaises(ValueError):
+            data = [None, 1]
+            self.calc.subtract(*data)
+
+
 if __name__ == '__main__':
     unittest.main()
